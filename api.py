0c7636fda2a11ac05df2ebd86aeb28e366552012
manoj-b-b <manojb.bhamsagar@draup.com>
2023-05-10 17:44:22 +0530

    handled exception

--- api.py
+++ api.py
@@ -1,15 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE', 'PATCH'])
 def log_headers():
-    print("Request Headers:")
-    for header, value in request.headers.items():
-        print(f"{header}: {value}")
+    try:
+        print("Request Headers:")
+        for header, value in request.headers.items():
+            print(f"{header}: {value}")
 
-    return "Headers logged."
+        return "Headers logged."
+    except Exception as e:
+        response = {
+            "error": str(e),
+            "type": type(e).__name__
+        }
+        return jsonify(response), 500
 
 
 if __name__ == '__main__':
